672f8b33adb9c8744f9596f946f8fd4be181abb7
Dinh Anh Dung <dinhanhdung1996@gmail.com>
2020-03-27 12:32:31 +0800

    save change

--- scripts/run1.sh
+++ scripts/run1.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 
-python metagan_mnist.py --model metagan9 --n_steps 300000
-python metagan_mnist.py --model metagan9 --is_train 0 --n_steps 300000
+python metagan_mnist.py --model metagan_test_nofakeaux1 --n_steps 400000
+python metagan_mnist.py --model metagan_test_nofakeaux1 --is_train 0 --n_steps 400000
 
 
-#python metagan_mnist.py --model metagan2 --n_steps 300000
-#python metagan_mnist.py --model metagan2 --is_train 0
+
+#python metagan_mnist.py --model metagan11 --n_steps 400000
+#python metagan_mnist.py --model metagan11 --is_train 0 --n_steps 400000
