7fb7c56f5f39736806e8d40300b1194409c8815a
Dinh Anh Dung <dinhanhdung1996@gmail.com>
2020-03-27 19:53:53 +0800

    save change

--- scripts/run1.sh
+++ scripts/run1.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 
-python metagan_mnist.py --model metagan9 --n_steps 300000
-python metagan_mnist.py --model metagan9 --is_train 0 --n_steps 300000
+python metagan_mnist.py --model metagan9 --n_steps 400000
+python metagan_mnist.py --model metagan9 --is_train 0 --n_steps 400000
 
 
 #python metagan_mnist.py --model metagan2 --n_steps 300000
--- scripts/run2.sh
+++ scripts/run2.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 
-python metagan_mnist.py --model metagan5 --n_steps 300000
-python metagan_mnist.py --model metagan5 --is_train 0 --n_steps 300000
+python metagan_mnist.py --model metagan10 --n_steps 400000 --gpu_id 1
+python metagan_mnist.py --model metagan10 --is_train 0 --n_steps 400000 --gpu_id 1
 
 
 
