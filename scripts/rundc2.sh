b6db5556828bb3a16446a32c5ac72edd55eab507
Dinh Dung <dinhanhdung1996@gmail.com>
2020-03-27 13:18:00 +0800

    save change

---
+++ scripts/rundc2.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+python dcgan_mnist.py --model dcgan6 --n_steps 400000
+python dcgan_mnist.py --model dcgan6 --is_train 0 --n_steps 400000
+
+python dcgan_mnist.py --model dcgan7 --n_steps 400000
+python dcgan_mnist.py --model dcgan7 --is_train 0 --n_steps 400000
+
+python dcgan_mnist.py --model dcgan8 --n_steps 400000
+python dcgan_mnist.py --model dcgan8 --is_train 0 --n_steps 400000
+
+python dcgan_mnist.py --model dcgan9 --n_steps 400000
+python dcgan_mnist.py --model dcgan9 --is_train 0 --n_steps 400000
+
+python dcgan_mnist.py --model dcgan10 --n_steps 400000
+python dcgan_mnist.py --model dcgan10 --is_train 0 --n_steps 400000
+#python metagan_mnist.py --model metgan4
+#python metagan_mnist.py --model metagan4 --is_train 0
