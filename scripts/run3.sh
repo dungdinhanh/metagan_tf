c7b1d2c8629659ed7d85cd354a73201a635d87b9
Dinh Anh Dung <dinhanhdung1996@gmail.com>
2020-04-06 10:55:05 +0800

    save change

---
+++ scripts/run3.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+
+
+#python metagan_mnist.py --model metagan1 --n_steps 400000 --gpu_id 0 --lambda_ent 0.1
+#python metagan_mnist.py --model metagan1 --is_train 0 --n_steps 400000 --gpu_id 0 --lambda_ent 0.1
+python metagan_mnist_cp.py --model metagan1 --n_steps 400000 --gpu_id 1 --lambda_ent 0.1
+
+#python metagan_mnist.py --model metagan2 --n_steps 400000 --gpu_id 0 --lambda_ent 0.4
+#python metagan_mnist.py --model metagan2 --is_train 0 --n_steps 400000 --gpu_id 0 --lambda_ent 0.1
+#python metagan_mnist_cp.py --model metagan2 --n_steps 400000 --gpu_id 0 --lambda_ent 0.1
+#
+#python metagan_mnist.py --model metagan3_lambda0.4 --n_steps 400000 --gpu_id 0 --lambda_ent 0.4
+#python metagan_mnist.py --model metagan3_lambda0.4 --is_train 0 --n_steps 400000 --gpu_id 0 --lambda_ent 0.1
+#python metagan_mnist_cp.py --model metagan2 --n_steps 400000 --gpu_id 0 --lambda_ent 0.1
+
+#python metagan_mnist.py --model metagan4
+#python metagan_mnist.py --model metagan4 --is_train 0
--- scripts/run_no_fakeaux.sh
+++ scripts/run_no_fakeaux.sh
@@ -16,6 +16,12 @@ python metagan_mnist_test.py --model metagan3_nofaux --n_steps 400000 --lambda_e
 python metagan_mnist_test.py --model metagan3_nofaux --is_train 0 --n_steps 400000 --lambda_ent 0.6
 
 
+python metagan_mnist_test.py --model metagan3_nofaux --n_steps 400000 --lambda_ent 0.4
+python metagan_mnist_test.py --model metagan3_nofaux --is_train 0 --n_steps 400000 --lambda_ent 0.4
+
+python metagan_mnist_test.py --model metagan3_nofaux --n_steps 400000 --lambda_ent 0.2
+python metagan_mnist_test.py --model metagan3_nofaux --is_train 0 --n_steps 400000 --lambda_ent 0.2
+
 #python metagan_mnist.py --model metagan11 --n_steps 400000
 #python metagan_mnist.py --model metagan11 --is_train 0 --n_steps 400000
 
