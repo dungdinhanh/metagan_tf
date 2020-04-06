9bcb9f5d0239e770c36da471f6559c964528fc7d
Dinh Anh Dung <dinhanhdung1996@gmail.com>
2020-04-06 12:55:35 +0800

    save change

--- scripts/run2.sh
+++ scripts/run2.sh
@@ -8,12 +8,12 @@ python metagan_mnist.py --model metagan1_lambda0.4 --n_steps 400000 --gpu_id 1 -
 python metagan_mnist.py --model metagan1_lambda0.4 --is_train 0 --n_steps 400000 --gpu_id 1
 
 
-python metagan_mnist.py --model metagan2_lambda0.4 --n_steps 400000 --gpu_id 1 --lambda_ent 0.4
-python metagan_mnist.py --model metagan2_lambda0.4 --is_train 0 --n_steps 400000 --gpu_id 1 --lambda_ent 0.4
-
-
-python metagan_mnist.py --model metagan3_lambda0.4 --n_steps 400000 --gpu_id 1 --lambda_ent 0.4
-python metagan_mnist.py --model metagan3_lambda0.4 --is_train 0 --n_steps 400000 --gpu_id 1 --lambda_ent 0.4
+#python metagan_mnist.py --model metagan2_lambda0.4 --n_steps 400000 --gpu_id 1 --lambda_ent 0.4
+#python metagan_mnist.py --model metagan2_lambda0.4 --is_train 0 --n_steps 400000 --gpu_id 1 --lambda_ent 0.4
+#
+#
+#python metagan_mnist.py --model metagan3_lambda0.4 --n_steps 400000 --gpu_id 1 --lambda_ent 0.4
+#python metagan_mnist.py --model metagan3_lambda0.4 --is_train 0 --n_steps 400000 --gpu_id 1 --lambda_ent 0.4
 
 
 #python metagan_mnist.py --model metagan4
