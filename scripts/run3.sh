5887874a31cb48e2372dd5312467df0a65aa0787
Dinh Anh Dung <dinhanhdung1996@gmail.com>
2020-04-07 10:42:43 +0800

    save change

--- scripts/run3.sh
+++ scripts/run3.sh
@@ -3,9 +3,9 @@
 
 
 
-python metagan_mnist_nogradgen.py --model metagannge1 --n_steps 400000 --gpu_id 0 --lambda_ent 0.1
-python metagan_mnist_nogradgen.py --model metagannge1 --is_train 0 --n_steps 400000 --gpu_id 0 --lambda_ent 0.1
-python metagan_mnist_cp.py --model metagange1 --n_steps 400000 --gpu_id 1 --lambda_ent 0.1
+python metagan_mnist_nogradgen.py --model metagannge3 --n_steps 400000 --gpu_id 0 --lambda_ent 0.1
+python metagan_mnist_nogradgen.py --model metagannge3 --is_train 0 --n_steps 400000 --gpu_id 0 --lambda_ent 0.1
+python metagan_mnist_cp.py --model metagannge3 --n_steps 400000 --gpu_id 0 --lambda_ent 0.1
 
 #python metagan_mnist.py --model metagan2 --n_steps 400000 --gpu_id 0 --lambda_ent 0.4
 #python metagan_mnist.py --model metagan2 --is_train 0 --n_steps 400000 --gpu_id 0 --lambda_ent 0.1
