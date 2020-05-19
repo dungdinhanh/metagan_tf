53abb7f479399ba801d8af3169f347a3a8200dc5
Dinh Dung <dinhanhdung1996@gmail.com>
2020-05-19 16:46:05 +0800

    save change

--- dcgan_cifar.py
+++ dcgan_cifar.py
@@ -1,7 +1,7 @@
 import os, sys
 import numpy as np
 import argparse
-from   dcgan import DCGAN
+from   test_net.dcgan_general import DCGAN
 from   modules.dataset import Dataset
 from   modules.eval import compute_fid_score_mnist
 from   modules.fiutils import mkdirs
